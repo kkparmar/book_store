a21ad8799e48ba2d1f3ad2642ad5e1c54fed1da2
Kunal Parmar <parmar.kunal@gnail.com>
2024-06-29 14:08:53 +1000

    enable servies

---
+++ infrastructure/terraform/locals.tf
@@ -0,0 +1,9 @@
+locals {
+    enabled_aps = [
+        "firestore.googleapis.com",
+        "firebaserules.googleapis.com",
+        "storage-api.googleapis.com",
+        "artifactregistry.googleapis.com",
+        "containerregistry.googleapis.com"
+    ]
+}
--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -10,3 +10,11 @@ output "random_string" {
     value = random_string.random.id
 }
 
+### enable apis
+
+resource "google_project_service" "sevices" {
+  for_each = toset(local.enabled_apis)
+  project = var.project_id
+  service = each.value
+
+}
