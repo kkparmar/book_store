d74e4bdbfeb42acfe9279546fcbe4ea85a254591
Kunal Parmar <parmar.kunal@gnail.com>
2024-06-29 16:04:29 +1000

    fixed resoruce name

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -12,7 +12,7 @@ output "random_string" {
 
 ### enable apis
 
-resource "google_project_service" "services" {
+resource "google_project_service" "sevices" {
   for_each = toset(local.enabled_apis)
   project = var.project_id
   service = each.value
@@ -27,5 +27,5 @@ resource "google_firestore_database" "database" {
   concurrency_mode            = "OPTIMISTIC"
   app_engine_integration_mode = "DISABLED"
 
-  depends_on = [google_project_service.services["firestore.googleapis.com"]]
+  depends_on = [google_project_service.sevices["firestore.googleapis.com"]]
 }
